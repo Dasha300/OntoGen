2cd83da13a5024184976e321e009e9d966940370
Dasha Denisova <daryalich@mail.ru>
2021-04-13 18:49:24 +0800

    Program with damage Unicode

--- main.py
+++ main.py
@@ -4,15 +4,21 @@ import sys
 import ftfy
 import os
 
-#  print(ftfy.fix_text('HTML entities &lt;3'))
-
-
 def pathent(csv_path):
     if csv_path[len(csv_path) - 4:len(csv_path)] == '.csv':
         return csv_path
     else:
         return 1
 
+def unocodeftfy(json_path):
+    with open('input.json', 'r', encoding='utf-8') as f:
+        text = json.load(f)
+        i = -1
+        for strjson in text:
+            i = i + 1
+            for objjson in strjson:
+                text[i][objjson] = ftfy.fix_text(text[i][objjson])
+    openjsonfile(json_path)
 
 def csvent(csv_path, json_path):
     if csv_path[len(csv_path) - 4:len(csv_path)] != '.csv' or json_path[len(json_path) - 5:len(json_path)] != '.json':
@@ -20,45 +26,35 @@ def csvent(csv_path, json_path):
     else:
         return sys.exit()
 
+def opencsvfile(csv_path):
+    with open(csv_path) as f:
+        reader = csv.DictReader(f, skipinitialspace=True, delimiter=',')
+        rows = list(reader)
+    return rows
+
+def openjsonfile(json_path):
+    with open(json_path, 'w') as f:
+        json.dump(rows, f, indent=4)
 
 path = input()
 
-#  path = ['F:/test']
 if os.path.exists(path):
     print('Такой путь существует: ', path)
     for dirs, folder, files in os.walk(path):
-        print('Выбранный каталог: ', dirs)
-        print('Вложенные папки: ', folder)
-        print('Файлы в папке: ', files)
-        print('\n')
         for awhile in files:
             if (pathent(awhile) == 1):
-                print("Не csv")
                 continue
             else:
                 csv_path = awhile
                 json_path = awhile[0:len(awhile) - 4] + '.json'
-
-                with open(csv_path) as f:
-                    reader = csv.DictReader(f, skipinitialspace=True, delimiter=',')
-                    rows = list(reader)
-
-                with open(json_path, 'w') as f:
-                    json.dump(rows, f, indent=4)
-                print(json_path)
+                rows = opencsvfile(csv_path)
+                openjsonfile(json_path)
 else:
     print('Такого пути нет', path)
 
 csv_path = input()
 json_path = input()
 csvent(csv_path, json_path)
-
-#  csv_path = 'input.csv'
-#  json_path = 'output.json'
-
-with open(csv_path) as f:
-    reader = csv.DictReader(f, skipinitialspace=True, delimiter=',')
-    rows = list(reader)
-
-with open(json_path, 'w') as f:
-    json.dump(rows, f, indent=4)
+rows = opencsvfile(csv_path)
+openjsonfile(json_path)
+unocodeftfy(json_path)
